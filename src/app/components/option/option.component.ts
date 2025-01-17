98cfab796009782a2943854ebd488d03c8002606
Arthur Axiotes <axiotesarthur@gmail.com>
2025-01-17 20:23:49 -0300

    feat: componente de opcoes dinamico

--- src/app/components/option/option.component.ts
+++ src/app/components/option/option.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-option',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './option.component.html',
-  styleUrl: './option.component.scss'
+  styleUrl: './option.component.scss',
 })
 export class OptionComponent {
-
+  @Input() public name!: string;
+  @Input() public details!: string;
+  @Input() public checked!: boolean;
 }
