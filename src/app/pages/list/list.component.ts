1e0a713a59b2946dc3388e0d19d3112ed9899156
Arthur Axiotes <axiotesarthur@gmail.com>
2025-01-17 20:14:04 -0300

    feat: criacao do componente de opcao

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { OptionComponent } from "../../components/option/option.component";
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [OptionComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
