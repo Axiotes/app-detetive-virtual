5c4d95cfd1bd2fda663c365ebe80f08e128a8418
Arthur Axiotes <axiotesarthur@gmail.com>
2025-01-18 14:35:31 -0300

    feat: criacao do componente de palpite
    feat: definicao de rota e acessar parametro de rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './pages/list/list.component';
+import { GuessComponent } from './pages/guess/guess.component';
 
 export const routes: Routes = [
-    { path: 'list', component: ListComponent }
+  { path: 'list', component: ListComponent },
+  { path: 'guess/:evidence', component: GuessComponent },
 ];
---
+++ src/app/pages/guess/guess.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SUSPECTS } from '../../../../data/suspects.mock';
+import { WEAPONS } from '../../../../data/weapons.mock';
+import { LOCATIONS } from '../../../../data/locations.mock';
+import { OptionComponent } from '../../components/option/option.component';
+import { NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-guess',
+  standalone: true,
+  imports: [OptionComponent, NgFor],
+  templateUrl: './guess.component.html',
+  styleUrl: './guess.component.scss',
+})
+export class GuessComponent implements OnInit {
+  public options!: any;
+  public title!: string;
+
+  constructor(private ActivatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const evidences: { [key: string]: () => void } = {
+      suspect: () => {
+        this.options = SUSPECTS;
+        this.title = 'Selecione um Suspeito';
+      },
+      weapon: () => {
+        this.options = WEAPONS;
+        this.title = 'Selecione uma Arma';
+      },
+      location: () => {
+        this.options = LOCATIONS;
+        this.title = 'Selecione um Local';
+      },
+    };
+
+    this.ActivatedRoute.params.subscribe((param) => {
+      const evidence = param['evidence'];
+      evidences[evidence]();
+    });
+  }
+}
