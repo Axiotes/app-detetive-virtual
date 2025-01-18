cd6c32fc3e627d0ce12f2eaf71a00e8631feef74
Arthur Axiotes <axiotesarthur@gmail.com>
2025-01-18 16:25:15 -0300

    feat: pagina de palpite dinamica

--- src/app/pages/guess/guess.component.ts
+++ src/app/pages/guess/guess.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { SUSPECTS } from '../../../../data/suspects.mock';
 import { WEAPONS } from '../../../../data/weapons.mock';
 import { LOCATIONS } from '../../../../data/locations.mock';
@@ -19,30 +19,46 @@ import { LucideIconData } from 'lucide-angular/icons/types';
 export class GuessComponent implements OnInit {
   public options!: Evidence[];
   public title!: string;
+  public lastRoute!: string;
+  public nextRoute!: string;
+
+  public evidences: { [key: string]: () => void } = {
+    suspect: () => {
+      this.options = SUSPECTS;
+      this.title = 'Selecione um Suspeito';
+      this.lastRoute = 'list';
+      this.nextRoute = 'guess/weapon';
+    },
+    weapon: () => {
+      this.options = WEAPONS;
+      this.title = 'Selecione uma Arma';
+      this.lastRoute = 'guess/suspect';
+      this.nextRoute = 'guess/location';
+    },
+    location: () => {
+      this.options = LOCATIONS;
+      this.title = 'Selecione um Local';
+      this.lastRoute = 'guess/weapon';
+      this.nextRoute = 'guess/confirm';
+    },
+  };
 
   public arrow: LucideIconData = ArrowLeft;
 
-  constructor(private ActivatedRoute: ActivatedRoute) {}
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
-    const evidences: { [key: string]: () => void } = {
-      suspect: () => {
-        this.options = SUSPECTS;
-        this.title = 'Selecione um Suspeito';
-      },
-      weapon: () => {
-        this.options = WEAPONS;
-        this.title = 'Selecione uma Arma';
-      },
-      location: () => {
-        this.options = LOCATIONS;
-        this.title = 'Selecione um Local';
-      },
-    };
-
-    this.ActivatedRoute.params.subscribe((param) => {
+    this.activatedRoute.params.subscribe((param) => {
       const evidence = param['evidence'];
-      evidences[evidence]();
+      this.evidences[evidence]();
     });
+  }
+
+  public next() {
+    this.router.navigateByUrl(this.nextRoute);
+  }
+
+  public back() {
+    this.router.navigateByUrl(this.lastRoute);
   }
 }
