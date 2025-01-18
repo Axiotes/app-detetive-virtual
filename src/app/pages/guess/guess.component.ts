7f643589e4ec0f3ca6e8c9239d16dff1578181fe
Arthur Axiotes <axiotesarthur@gmail.com>
2025-01-18 16:02:46 -0300

    feat: estilizacao da pagina de palpite

--- src/app/pages/guess/guess.component.ts
+++ src/app/pages/guess/guess.component.ts
@@ -6,17 +6,21 @@ import { LOCATIONS } from '../../../../data/locations.mock';
 import { OptionComponent } from '../../components/option/option.component';
 import { NgFor } from '@angular/common';
 import { Evidence } from '../../../types/evidence.type';
+import { ArrowLeft, LucideAngularModule } from 'lucide-angular';
+import { LucideIconData } from 'lucide-angular/icons/types';
 
 @Component({
   selector: 'app-guess',
   standalone: true,
-  imports: [OptionComponent, NgFor],
+  imports: [OptionComponent, NgFor, LucideAngularModule],
   templateUrl: './guess.component.html',
   styleUrl: './guess.component.scss',
 })
 export class GuessComponent implements OnInit {
   public options!: Evidence[];
   public title!: string;
+
+  public arrow: LucideIconData = ArrowLeft;
 
   constructor(private ActivatedRoute: ActivatedRoute) {}
 
